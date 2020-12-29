c28d3970c566b05551a158d07169b2087482f651
MitulKirti <kirtimitul1@email.com>
2020-12-29 10:19:01 +0530

    Updates

---
+++ precache-manifest.62957b53760ec9f7b91dd938519ee149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f75fdb59105403237df5070e06ad9cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca65d268e45acae992d9",
+    "url": "/static/css/main.c20f30ee.chunk.css"
+  },
+  {
+    "revision": "7bcd30fce7a4dd4f5a05",
+    "url": "/static/js/2.c06e8877.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c06e8877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca65d268e45acae992d9",
+    "url": "/static/js/main.d53a2550.chunk.js"
+  },
+  {
+    "revision": "d29e7945567353dd8bd6",
+    "url": "/static/js/runtime-main.1f840449.js"
+  }
+]);
